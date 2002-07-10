f4c501dd494925475d719e1982058441a352c7c9
Max Horn <max@quendi.de>
2002-07-10 18:20:27 +0000

    made the CVS daily build stuff a bit clearer; simplified the HTML
    svn path=/web/trunk/; revision=802

--- downloads.php
+++ downloads.php
@@ -29,62 +29,67 @@ echo html_round_frame_start("Downloads","98%","",20);
 	  Downloads are hosted with SourceForge.net. If you have one of the supported systems, you can directly download
 	  the appropriate binary distribution. If you have another system, download the source and read the
 	  <a href="http://cvs.sourceforge.net/cgi-bin/viewcvs.cgi/*checkout*/scummvm/scummvm/readme.txt?rev=HEAD">readme.txt</a>
-	  file for directions on how to build ScummVM.<br><br>
+	  file for directions on how to build ScummVM.
+	</p>
 
+	<p>
 	  If you have successfully ported ScummVM to a platform not listed, please drop us a note, telling which OS, etc.
-	  you used.<br><br>
+	  you used.
+	</p>
 
-	  You can also get the latest development source and binaries from the <a href="http://scummvm.sourceforge.net/daily/">CVS Daily Snapshot</a>
-	  page. To quickly get the binary executable from that page, download the .gz from the column of the OS you use. Then
+	<p>
+	  <b>Daily CVS Builds:</b>
+	</p>
+
+	<p>
+	  You can get the latest development source and binaries for BeOS/Intel and Linux/Intel from the
+	  <a href="http://scummvm.sourceforge.net/daily/">CVS Daily Snapshot</a> page. To quickly get a
+	  binary executable from that page, download the .gz from the column of the OS you use. Then
 	  simply rename the file to it&#146;s executable form.<br>
 
 	  View the <a href="/daily/ChangeLog">ChangeLog</a> to see the latest updates of ScummVM
 	</p>
 	
 	<p>
-	  <b>Daily CVS Builds</b>
-	  <ul>
-	    <li><a href="/downloads/scummvmwin32.exe">Win32 Daily Snapshot</a> <small>
-	    (<? echo intval(filesize("downloads/scummvmwin32.exe")/1024) ?>K exe file, last update:
-	    <? echo date("F j, Y, g:i a",filemtime("downloads/scummvmwin32.exe")); ?>)</small></li>	  
-	  </ul>
+	  There is also the <a href="/downloads/scummvmwin32.exe">Win32 Daily Snapshot</a> <small>
+	  (<? echo intval(filesize("downloads/scummvmwin32.exe")/1024) ?>K exe file, last update:
+	  <? echo date("F j, Y, g:i a",filemtime("downloads/scummvmwin32.exe")); ?>)</small></li>	  
 	</p>
 	
 	<p>
  	  <b>0.2.0 Release binaries:</b>
-	  <ul>
-	    <li><a href="http://prdownloads.sourceforge.net/scummvm/scummvm_0.2.0-win32.zip">Win32 Official 0.2.0 Build</a> <small>(260k zip file)</small></li>
-	    <li><a href="http://prdownloads.sourceforge.net/scummvm/scummvm_0.2.0_i386.deb">Woody Official 0.2.0 Package</a> <small>(212k deb package)</small></li>
-	    <li><a href="http://prdownloads.sourceforge.net/scummvm/scummvm-0.2.0-macosx_sdl.tar.gz">MacOS X Official 0.2.0 Build</a> <small>(359k gziped tarball)</small></li>
-	    <li><a href="http://prdownloads.sourceforge.net/scummvm/scummvm_0.2-3_arm.ipk">iPaq Linux Official 0.2.0 Build</a> <small>(250k ipkg package)</small></li>
-	    <li><a href="http://prdownloads.sourceforge.net/scummvm/scummvm-mips-sgi-irix5.gz">SGI Irix MIPS Official 0.2.0 binary</a> <small>(516kb gzip file)</small></li>
-	    <li><a href="http://prdownloads.sourceforge.net/scummvm/PocketSCUMM_Install_0_2_0.exe">WinCE ARM/MIPS/SH3 Official 0.2.0 installer</a> <small>(820k Win32 executable)</small></li>
-	    <li><a href="http://prdownloads.sourceforge.net/scummvm/scummvm-0.2.0-dc-bin.zip">Dreamcast Official 0.2.0 scrambled binary</a> <small>(338k zip file)</small></li>
-	    <li><a href="http://prdownloads.sourceforge.net/scummvm/scummvm-0.2.0-dc-nero.zip">Dreamcast Official 0.2.0 NERO image</a> <small>(2.3mb zip file)</small></li>
-	    <li>more builds (eg, BeOS, RPMs, MorphOS) coming soon..</li>
-	  </ul>
 	</p>
+	<ul>
+	  <li><a href="http://prdownloads.sourceforge.net/scummvm/scummvm_0.2.0-win32.zip">Win32 Official 0.2.0 Build</a> <small>(260k zip file)</small></li>
+	  <li><a href="http://prdownloads.sourceforge.net/scummvm/scummvm_0.2.0_i386.deb">Woody Official 0.2.0 Package</a> <small>(212k deb package)</small></li>
+	  <li><a href="http://prdownloads.sourceforge.net/scummvm/scummvm-0.2.0-macosx_sdl.tar.gz">MacOS X Official 0.2.0 Build</a> <small>(359k gziped tarball)</small></li>
+	  <li><a href="http://prdownloads.sourceforge.net/scummvm/scummvm_0.2-3_arm.ipk">iPaq Linux Official 0.2.0 Build</a> <small>(250k ipkg package)</small></li>
+	  <li><a href="http://prdownloads.sourceforge.net/scummvm/scummvm-mips-sgi-irix5.gz">SGI Irix MIPS Official 0.2.0 binary</a> <small>(516kb gzip file)</small></li>
+	  <li><a href="http://prdownloads.sourceforge.net/scummvm/PocketSCUMM_Install_0_2_0.exe">WinCE ARM/MIPS/SH3 Official 0.2.0 installer</a> <small>(820k Win32 executable)</small></li>
+	  <li><a href="http://prdownloads.sourceforge.net/scummvm/scummvm-0.2.0-dc-bin.zip">Dreamcast Official 0.2.0 scrambled binary</a> <small>(338k zip file)</small></li>
+	  <li><a href="http://prdownloads.sourceforge.net/scummvm/scummvm-0.2.0-dc-nero.zip">Dreamcast Official 0.2.0 NERO image</a> <small>(2.3mb zip file)</small></li>
+	</ul>
 	
 	<p>
 	  <b>0.2.0 Source Code:</b>
-	  <ul>
-	    <li><a href="http://prdownloads.sourceforge.net/scummvm/scummvm_0.2.0-src.tgz">Source tarball</a> <small>(668k)</small></li>
-	  </ul>
 	</p>
+	<ul>
+	  <li><a href="http://prdownloads.sourceforge.net/scummvm/scummvm_0.2.0-src.tgz">Source tarball</a> <small>(668k)</small></li>
+	</ul>
 	
 	<p>
 	  <b>Required Libraries:</b>
-	  <ul>
-	    <li><a href="http://www.libsdl.org/download-1.2.html">SDL 1.2.x</a></li>
-	  </ul>
-	</p>
+    </p>
+	<ul>
+	  <li><a href="http://www.libsdl.org/download-1.2.html">SDL 1.2.x</a></li>
+	</ul>
 		
 	<p>
 	  <b>Optional Libraries:</b>
-	  <ul>
-	    <li><a href="http://www.mars.org/home/rob/proj/mpeg/">MAD</a>: MPEG Audio Decoder</li>
-	  </ul>
-	</p>		
+	</p>
+	<ul>
+	  <li><a href="http://www.mars.org/home/rob/proj/mpeg/">MAD</a>: MPEG Audio Decoder</li>
+	</ul>
 		
 <?
 
