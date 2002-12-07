9608d0393dbd902a55fa303a504bdde9a6888f4a
James Brown <ender@users.sourceforge.net>
2002-12-07 01:53:26 +0000

    Update download page for 0.3.0b
    svn path=/web/trunk/; revision=932

--- downloads.php
+++ downloads.php
@@ -21,7 +21,7 @@ echo html_round_frame_start("Downloads","98%","",20);
 
 ?>
 	<p>
-	  <big><b>Downloads for ScummVM</b> ver: 0.2.0</big><br>
+	  <big><b>Downloads for ScummVM</b> ver: 0.3.0b</big><br>
 	  <? echo html_line(); ?>
 	</p>
 
@@ -50,8 +50,6 @@ echo html_round_frame_start("Downloads","98%","",20);
 
 	  View the <a href="/daily/ChangeLog">ChangeLog</a> to see the latest updates of ScummVM
 -->
-	Please note that the 0.2.0 download linked below is several months old, and not recommended for use. We are planning a new 
-release soon, but meanwhile here are links to experimental snapshots of the current code:
 
 	  <ul>
 	  <li><a href="/downloads/scummvmwin32.exe">Win32 Daily Snapshot</a> <small> (<? echo intval(filesize("downloads/scummvmwin32.exe")/1024) ?>K exe file, last update: <? echo date("F j, Y, g:i a",filemtime("downloads/scummvmwin32.exe")); ?>)</small></li>
@@ -60,24 +58,44 @@ release soon, but meanwhile here are links to experimental snapshots of the curr
 	</p>
 	
 	<p>
- 	  <b>0.2.0 Release binaries:</b>
+ 	  <b>0.3.0b Release binaries:</b>
+	  <i><center>For a list of changes since the last version (0.2.0), <a href="http://sourceforge.net/project/shownotes.php?release_id=126358">read the release notes</a></center></i>
 	</p>
+
 	<ul>
-	  <li><a href="http://prdownloads.sourceforge.net/scummvm/scummvm_0.2.0-win32.zip">Win32 Official 0.2.0 Build</a> <small>(260k zip file)</small></li>
-	  <li><a href="http://prdownloads.sourceforge.net/scummvm/scummvm_0.2.0_i386.deb">Woody Official 0.2.0 Package</a> <small>(212k deb package)</small></li>
-	  <li><a href="http://prdownloads.sourceforge.net/scummvm/scummvm-0.2.0-macosx_sdl.tar.gz">MacOS X Official 0.2.0 Build</a> <small>(359k gziped tarball)</small></li>
-	  <li><a href="http://prdownloads.sourceforge.net/scummvm/scummvm_0.2-3_arm.ipk">iPaq Linux Official 0.2.0 Build</a> <small>(250k ipkg package)</small></li>
-	  <li><a href="http://prdownloads.sourceforge.net/scummvm/scummvm-mips-sgi-irix5.gz">SGI Irix MIPS Official 0.2.0 binary</a> <small>(516kb gzip file)</small></li>
-	  <li><a href="http://prdownloads.sourceforge.net/scummvm/PocketSCUMM_Install_0_2_0.exe">WinCE ARM/MIPS/SH3 Official 0.2.0 installer</a> <small>(820k Win32 executable)</small></li>
-	  <li><a href="http://prdownloads.sourceforge.net/scummvm/scummvm-0.2.0-dc-bin.zip">Dreamcast Official 0.2.0 scrambled binary</a> <small>(338k zip file)</small></li>
-	  <li><a href="http://prdownloads.sourceforge.net/scummvm/scummvm-0.2.0-dc-nero.zip">Dreamcast Official 0.2.0 NERO image</a> <small>(2.3mb zip file)</small></li>
+	  <li><a href="http://prdownloads.sourceforge.net/scummvm/scummvm_0.3.0b-win32.exe?download">ScummVM Windows Installer</a> <small>(1.0M Win32 .exe)</small></li>
+	  <li><a href="http://prdownloads.sourceforge.net/scummvm/scummvm_0.3.0b-win32.zip?download">ScummVM Windows Zipfile</a> <small>(985k zipfile)</small></li>
+	  <li><a href="http://prdownloads.sourceforge.net/scummvm/scummvm-0.3.0b-macosx.dmg?download">MacOS X Disk Image</a> <small>(627k dmg image)</small></li>
+	  <li><a href="http://prdownloads.sourceforge.net/scummvm/scummvm_0.3.0b-morphos.tar.bz2?download">MorphOS binary</a> <small>(397k tar.bz2)</small></li>
+	  <li><a href="http://prdownloads.sourceforge.net/scummvm/scummvm_0.3.0b-0.woody.1_i386.deb?download">Debian Stable (woody) Package</a> <small>(321k .deb)</small></li>
+	  <li><a href="http://prdownloads.sourceforge.net/scummvm/scummvm-0.3.0b-redhat80.i386.rpm?download">RedHat 8.0 package</a> <small>(441k RPM)</small></li>
+	  <li><hr></li>
+	  <li><a href="http://prdownloads.sourceforge.net/scummvm/PocketSCUMM_Install_0_3_0b.exe?download">PocketSCUMM Windows-based Installer (PocketPC: ARM/SH3/MIPS)</a> <small>(1.1M Win32 .exe)</small></li>
+	  <li><a href="http://prdownloads.sourceforge.net/scummvm/PocketSCUMM_0_3_0b-ARM.zip?download">PocketSCUMM ARM binary</a> <small>(371k .zip)</small></li>
+	  <li><a href="http://prdownloads.sourceforge.net/scummvm/PocketSCUMM_0_3_0b-MIPS.zip?download">PocketSCUMM MIPS binary</a> <small>(437k .zip)</small></li>
+	  <li><a href="http://prdownloads.sourceforge.net/scummvm/PocketSCUMM_0_3_0b-SH3.zip?download">PocketSCUMM SH3 binary</a> <small>(405k .zip)</small></li>
 	</ul>
 	
 	<p>
-	  <b>0.2.0 Source Code:</b>
+	  <b>0.3.0b Tools (mostly unsupported):</b>
 	</p>
 	<ul>
-	  <li><a href="http://prdownloads.sourceforge.net/scummvm/scummvm_0.2.0-src.tgz">Source tarball</a> <small>(668k)</small></li>
+	  <li><a href="http://prdownloads.sourceforge.net/scummvm/scummvm_tools_0.3.0b-win32.exe?download">ScummVM Tools - Windows Installer</a> <small>(159k Win32 .exe)</small></li>
+	  <li><a href="http://prdownloads.sourceforge.net/scummvm/scummvm_tools_0.3.0b-win32.zip?download">ScummVM Tools - Windows Zipfile</a> <small>(119k zip file)</small></li>
+	  <li><a href="http://prdownloads.sourceforge.net/scummvm/scummvm-tools-0.3.0-macosx.tar.gz?download">ScummVM Tools - MacOS X tarball</a> <small>(147k tar.gz)</small></li>
+	</ul>
+
+	<p>
+	  <b>0.3.0b Source Code:</b>
+	</p>
+	<ul>
+	  <li><a href="http://prdownloads.sourceforge.net/scummvm/scummvm_0.3.0b-src.zip?download">Source Zipfile</a> <small>(868k)</small></li>
+	  <li><a href="http://prdownloads.sourceforge.net/scummvm/scummvm_0.3.0b-src.tar.gz?download">Source tar.gz</a> <small>(705k)</small></li>
+	  <li><a href="http://prdownloads.sourceforge.net/scummvm/scummvm_0.3.0b-src.tar.bz2?download">Source tar.bz2</a> <small>(585k)</small>
+	  <li><hr></li>
+	  <li><a href="http://prdownloads.sourceforge.net/scummvm/scummvm_tools_0.3.0b-src.zip?download">Tools - Source zipfile</a> <small>(82k)</small>
+	  <li><a href="http://prdownloads.sourceforge.net/scummvm/scummvm_tools_0.3.0b-src.tar.gz?download">Tools - Source tar.gz</a> <small>(65k)</small>
+	  <li><a href="http://prdownloads.sourceforge.net/scummvm/scummvm_tools_0.3.0b-src.tar.bz2?download">Tools - Source tar.bz2</a> <small>(36k)</small>
 	</ul>
 	
 	<p>
