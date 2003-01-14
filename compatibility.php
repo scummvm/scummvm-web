20c36b31d8eeacb1e3efc8e05ca70dff7d8aa693
Travis Howell <kirben@optusnet.com.au>
2003-01-14 05:38:27 +0000

    Update simon
    svn path=/web/trunk/; revision=983

--- compatibility.php
+++ compatibility.php
@@ -79,7 +79,9 @@ $games = array(
 		'Curse of Monkey Island'				=> array('8','comi','30'),
 		'Simon The Sorcerer 1 Talkie (Win)'       		=> array('n/a','simon1win','85'),
 		'Simon The Sorcerer 1 Talkie (Dos)'       		=> array('n/a','simon1talkie','85'),
+		'Simon The Sorcerer 1 Talkie (Amiga CD32)'     		=> array('n/a','simon1cd32','5'),
 		'Simon The Sorcerer 1 (Dos)'           			=> array('n/a','simon1dos','80'),
+		'Simon The Sorcerer 1 (Amiga)'          		=> array('n/a','simon1amiga','0'),
 		'Simon The Sorcerer 1 (DEMO)'          			=> array('n/a','simon1demo','80'),
 		'Simon The Sorcerer 2 Talkie (Win)'       		=> array('n/a','simon2win','80'),
 		'Simon The Sorcerer 2 Talkie (Dos)'       		=> array('n/a','simon2talkie','80'),
@@ -132,10 +134,12 @@ $notes = array(
                    "<br>- Various minor graphical glitches",
 "simon1talkie" 	=> "Game is completable.".
                    "<br>- Various minor graphical glitches",
+"simon1cd32" 	=> "Not at all implemented yet - ScummVM doesn't understand the pkd, music, sfx and speech formats",
 "simon1dos" 	=> "Game is completable.".
                    "<br>- Freezes briefly when Swampling leaves his house".
                    "<br>- Freezes briefly when talking to demons in Sordid's Tower".
                    "<br>- Various minor graphical glitches",
+"simon1amiga" 	=> "Not at all implemented yet - ScummVM doesn't understand the pkd and music formats",
 "simon1demo" 	=> "Game demo is completable".
                    "<br>- Freezes briefly when Swampling leaves his house".
                    "<br>- No music",
