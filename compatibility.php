6be9bd591d8a61e087cecf3d3b7bab9fcdd809cb
Travis Howell <kirben@optusnet.com.au>
2003-05-27 09:12:41 +0000

    Update maniac/zak
    svn path=/web/trunk/; revision=1191

--- compatibility.php
+++ compatibility.php
@@ -56,8 +56,8 @@ else
 $games = array(
 		'Maniac Mansion (C64)'					=> array('1','maniacc64','0'),
 		'Zak McKracken and the Alien Mindbenders (C64)'		=> array('1','zakc64','0'),
-		'Maniac Mansion'					=> array('2','maniac','50'),
-		'Zak McKracken and the Alien Mindbenders'		=> array('2','zak','50'),
+		'Maniac Mansion'					=> array('2','maniac','53'),
+		'Zak McKracken and the Alien Mindbenders'		=> array('2','zak','53'),
 		'Indiana Jones and the Last Crusade'			=> array('3','indy3ega','80'),
 		'Indiana Jones and the Last Crusade (256)'		=> array('3','indy3','80'),
 		'Zak McKracken and the Alien Mindbenders (256 - FmTowns)' => array('3','zak256','85'),
@@ -102,9 +102,11 @@ $notes = array(
 "maniacc64" 	=> "Not at all implemented yet - ScummVM doesn't understand the non-blocked LFL format",
 "zakc64"	=> "Not at all implemented yet - ScummVM doesn't understand the non-blocked LFL format. Try Zak256 version",
 "maniac"	=> "Enhanced version runs, although in-game there are still many things wrong".
-                   "<br>- No music or sound effects",
+                   "<br>- Introduction music doesn't auto stop, if introduction is skipped when music is playing".
+                   "<br>- Some sound effects missing",
 "zak"		=> "Enhanced version runs, although in-game there are still many things wrong".
-                   "<br>- No music or sound effects",
+                   "<br>- Introduction music doesn't auto stop, if introduction is skipped when music is playing".
+                   "<br>- Some sound effects missing",
 "indy3ega"	=> "Game is completable".
                    "<br>- No music or sound effects",
 "indy3"         => "Game is completable" .
