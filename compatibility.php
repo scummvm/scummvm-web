a9a9d068af6d4f9ec924108b20059de0a1791f56
Travis Howell <kirben@optusnet.com.au>
2003-06-06 10:59:06 +0000

    Update maniac/zak targets
    svn path=/web/trunk/; revision=1234

--- compatibility.php
+++ compatibility.php
@@ -54,8 +54,6 @@ else
 
 // This Array Defines the games and thier ratings, etc.
 $games = array(
-		'Maniac Mansion (C64)'					=> array('1','maniac64','5'),
-		'Zak McKracken and the Alien Mindbenders (C64)'		=> array('1','zak64','0'),
 		'Maniac Mansion'					=> array('2','maniac','53'),
 		'Zak McKracken and the Alien Mindbenders'		=> array('2','zak','53'),
 		'Indiana Jones and the Last Crusade'			=> array('3','indy3ega','80'),
@@ -99,16 +97,16 @@ $games = array(
 	      );
 
 $notes = array(
-"maniac64" 	=> "Crashes when the next room is entered".
-                   "<br>- No actors shown".
-                   "<br>- No music or sound effects",
-"zak64"		=> "Not at all implemented yet - ScummVM doesn't understand the non-blocked LFL format. Try Zak256 version",
-"maniac"	=> "Enhanced version runs, although in-game there are still many things wrong".
-		   "<br>- Both Amiga and enchanced PC versions supported by this target".
-		   "<br>- No music or sound effects with Amiga version",
-"zak"		=> "Enhanced version runs, although in-game there are still many things wrong".
-		   "<br>- Both Amiga and enchanced PC versions supported by this target".
-		   "<br>- No music or sound effects with Amiga version",
+"maniac"	=> "Enhanced PC version runs, although in-game there are still many things wrong".
+		=> "Classic PC version crashes when entering the next room".
+		   "<br>- Both Amiga and PC versions supported by this target".
+		   "<br>- No actors shown in Classic PC version".
+		   "<br>- No music or sound effects with Amiga and Classic PC versions",
+"zak"		=> "Enhanced PC version runs, although in-game there are still many things wrong".
+		=> "Classic PC version is untested".
+		   "<br>- Both Amiga and PC versions supported by this target".
+		   "<br>- No actors shown in Classic PC version".
+		   "<br>- No music or sound effects with Amiga and Classic PC versions",
 "indy3ega"	=> "Game is completable".
 		   "<br>- Both Amiga (Use Amiga option) and PC versions supported by this target".
 		   "<br>- No music or sound effects with Amiga version",
