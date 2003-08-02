fa07943d775b9f2f59ff4011d5ba8e199dfed7d0
Max Horn <max@quendi.de>
2003-08-02 23:02:16 +0000

    added Morphos download
    svn path=/web/trunk/; revision=1328

--- downloads.php
+++ downloads.php
@@ -70,7 +70,7 @@ echo html_round_frame_start("Downloads","98%","",20);
  	  <b>0.5.0 Release binaries:</b>
 	</p>
 	<p>For a list of changes since the previous version, <a href="https://sourceforge.net/project/shownotes.php?release_id=175327">read the release notes</a>.</p>
-	<p>0.5.0 should be apt-get'able from Debian Unstable (sid) later in the week. Debian Stable (woody) downloads will also be added later in the week, as will downloads for BeOS and MorphOS</p>
+	<p>0.5.0 should be apt-get'able from Debian Unstable (sid) later in the week. Debian Stable (woody) downloads and a BeOS binary will also be added later in the week</p>
 
 	<ul>
 	  <li><a href="http://prdownloads.sourceforge.net/scummvm/scummvm-0.5.0-win32.exe?download">Windows Installer</a> <small>(1.0M Win32 .exe)</small></li>
@@ -80,9 +80,10 @@ echo html_round_frame_start("Downloads","98%","",20);
 	  <li><a href="http://prdownloads.sourceforge.net/scummvm/scummvm-0.5.0-1.src.rpm?download">Source RPM</a> <small>(1.3M RPM)</small>
 <!--
 	  <li><a href="http://prdownloads.sourceforge.net/scummvm/scummvm_0.4.1-0woody1_i386.deb?download">Debian Stable (woody) Package</a> <small>(377k .deb)</small></li>
-	  <li><a href="http://prdownloads.sourceforge.net/scummvm/scummvm-0.3.0-morphos.tar.bz2?download">MorphOS binary</a> <small>(397k .tar.bz2)</small></li>
 	  <li><a href="http://prdownloads.sourceforge.net/scummvm/scummvm-0.4.1-beos.pkg?download">BeOS package</a> <small>(2.9M pkg)</small></li>
 -->
+	  <li><a href="http://prdownloads.sourceforge.net/scummvm/scummvm-0.5.0-morphos.tgz ?download">MorphOS binary</a> <small>(779k .tar.gz)</small></li>
+
 	  <li><a href="http://prdownloads.sourceforge.net/scummvm/scummvm-0.5.0-palmos.zip?download">PalmOS binary</a> <small>(332k zipfile)</small></li>
 	  <li><a href="http://prdownloads.sourceforge.net/scummvm/scummvm-0.5.0-solaris8-sparc.tar.gz?download">Solaris 8 (Sparc) binary</a> <small>(797k .tar.gz)</small></li>
 	  <li><a href="http://prdownloads.sourceforge.net/scummvm/scummvm-0.5.0-dreamcast-plainfiles.tar.bz2?download">Dreamcast plain files</a> <small>(761k zipfile)</small></li>
