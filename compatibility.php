42e6f28d0428a64f7c97044bf62b79225cd7fc8c
Oliver Kiehl <olki@users.sourceforge.net>
2003-09-14 22:38:00 +0000

    sword2 now works on BE systems
    svn path=/web/trunk/; revision=1428

--- compatibility.php
+++ compatibility.php
@@ -235,7 +235,6 @@ $notes = array(
 		   "<br>- The fonts for the LINC terminal are partially incorrect and the text sometimes passes the screen borders".
 		   "<br>- Special characters for french and italian subtitles are incorrect sometimes",
 "sword2"	 => "Game is completable, with several minor glitches".
-		   "<br>- Does not work on big endian systems yet".
 		   "<br>- No cutscenes",
 "simon1win" 	=> "No known problems - game is completable.",
 "simon1talkie" 	=> "Game is completable.".
