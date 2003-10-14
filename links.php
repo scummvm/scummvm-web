d3ad1b19b09a3ae4b0c79c68dcf9afccb24d7a2b
Max Horn <max@quendi.de>
2003-10-14 13:30:08 +0000

    HTML 4.01 strict & cleanup (more work needed)
    svn path=/web/trunk/; revision=1492

--- links.php
+++ links.php
@@ -21,42 +21,46 @@ echo html_round_frame_start("Links","");
 ?>
 	<h1>Links</h1>
 	
+	<p>
+	  <img src="images/scummvm-link.png" width="88" height="31" alt="ScummVM" style="vertical-align: middle; text-align: left; margin: 5px;">
+	  <b>Link to us: </b>
+	  If you want to link your site to us: please feel free to use this image.
+	</p>
+
 	<h2>Official ports</h2>
 	<p>Some additional links for ports that have been merged into our CVS</p>
 
-	<blockquote>
-	<table border=0 cellpadding=5 cellspacing=0>
+	<table border=0 cellpadding=5 cellspacing=0 style="margin-left: 3em;">
 
 	  <tr><td><a href="http://arisme.free.fr/PocketScumm/index.php"><b>PocketScumm port</b></a></td></tr>
 	  <tr>
-	    <td><blockquote>
+	    <td style="padding-left: 3em;">
 	    Information about the PocketScumm port.
-	    </blockquote></td>
+	    </td>
 	  </tr>
 
 	  <tr><td><a href="http://forums.pocketmatrix.com/viewforum.php?f=20"><b>PocketScumm forum</b></a></td></tr>
 	  <tr>
-	    <td><blockquote>
+	    <td style="padding-left: 3em;">
 	    For help and discussion relating to PocketScumm.
-	    </blockquote></td>
+	    </td>
 	  </tr>
 
 	  <tr><td><a href="http://capers.free.fr/ScummVM/index.php"><b>PalmOS port</b></a></td></tr>
 	  <tr>
-	    <td><blockquote>
+	    <td style="padding-left: 3em;">
 	    News and information about the PalmOS port.
-	    </blockquote></td>
+	    </td>
 	  </tr>
 
 	  <tr><td><a href="http://people.inf.elte.hu/ph0x/gpscumm/"><b>GP32 port</b></a></td></tr>
 	  <tr>
-	    <td><blockquote>
+	    <td style="padding-left: 3em;">
 	    Information about the GP32 port.
-	    </blockquote></td>
+	    </td>
 	  </tr>
 
 	</table>
-	</blockquote>
 	
 	<!-- -->
 
@@ -65,24 +69,22 @@ echo html_round_frame_start("Links","");
 	CVS, but for the following this has not (yet) happened for various reasons. Note that the ScummVM team does not endorse
 	any of these ports. We did not test them, and we do not guarantee that they work properly. <span style="color: red">Use at your own risk!</span></p>
 
-	<blockquote>
-	<table border=0 cellpadding=5 cellspacing=0>
+	<table border=0 cellpadding=5 cellspacing=0 style="margin-left: 3em;">
 
 	  <tr><td><a href="http://www.sebelinteractive.de/scummvm/"><b>Amiga ports</b></a></td></tr>
 	  <tr>
-	    <td><blockquote>
+	    <td style="padding-left: 3em;">
 	    News site containing links and news for unofficial Amiga ports.
-	    </blockquote></td>
+	    </td>
 	  </tr>
 
 	  <tr><td><a href="http://dreo.org/p800/escummvm/index.html"><b>P800 port</b></a></td></tr>
 	  <tr>
-	    <td><blockquote>
+	    <td style="padding-left: 3em;">
 	    An unofficial port to the Sony Ericsson P800 running Symbian 7.0.
-	    </blockquote></td>
+	    </td>
 	  </tr>
 	</table>
-	</blockquote>
 
 	<!-- -->
 
@@ -90,166 +92,160 @@ echo html_round_frame_start("Links","");
 	<p>The following lists some libraries and technologies ScummVM makes use of
 	(depending on which system your run it and which configuration is chosen).</p>
 
-	<blockquote>
-	<table border=0 cellpadding=5 cellspacing=0>
+	<table border=0 cellpadding=5 cellspacing=0 style="margin-left: 3em;">
 
 	  <tr><td><a href="http://www.libsdl.org/"><b>SDL</b></a></td></tr>
 	  <tr>
-	    <td><blockquote>
+	    <td style="padding-left: 3em;">
 	    SDL (Simple DirectMedia Layer) is a cross-platform multimedia library, and used by
 	    the primary backend of ScummVM.
-	    </blockquote></td>
+	    </td>
 	  </tr>
 
 	  <tr><td><a href="http://www.underbit.com/products/mad/"><b>MAD</b></a></td></tr>
 	  <tr>
-	    <td><blockquote>
+	    <td style="padding-left: 3em;">
 	    MAD is a high-quality MPEG (MP3) audio decoder.
 	    ScummVM optionally supports playback of CD tracks and other audio data encoded using MP3.
-	    </blockquote></td>
+	    </td>
 	  </tr>
 
 	  <tr><td><a href="http://www.xiph.org/ogg/vorbis/"><b>Ogg Vorbis</b></a></td></tr>
 	  <tr>
-	    <td><blockquote>
+	    <td style="padding-left: 3em;">
 	    Ogg Vorbis is a fully open, non-proprietary, patent-and-royalty-free, general-purpose compressed audio format.
 	    ScummVM optionally supports playback of CD tracks and other audio data encoded using Ogg Vorbis.
-	    </blockquote></td>
+	    </td>
 	  </tr>
 
 	  <tr><td><a href="http://scale2x.sourceforge.net/"><b>Scale2x/3x/4x</b></a></td></tr>
 	  <tr>
-	    <td><blockquote>
+	    <td style="padding-left: 3em;">
 	    Scale2x is a real-time graphics effect able to increase the size of small bitmaps guessing the missing pixels without blurring the images.
 	    ScummVM optionally supports enlarging the game graphics using this scaler.
-	    </blockquote></td>
+	    </td>
 	  </tr>
 
 	  <tr><td><a href="http://www.hiend3d.com/hq3x.html"><b>hq2x/3x/4x</b></a></td></tr>
 	  <tr>
-	    <td><blockquote>
+	    <td style="padding-left: 3em;">
 	    The hq2x/3x/4x filters form a family of fast, high-quality magnification filters.
 	    ScummVM optionally supports enlarging the game graphics using these scaler.
-	    </blockquote></td>
+	    </td>
 	  </tr>
 
 	</table>
-	</blockquote>
 
 	<!-- -->
 
 	<h2>Other sites of interest</h2>
 	<p>The following are links to sites that provide news and help on retro-gaming or are otherwise of interest.</p>
 
-	<blockquote>
-	<table border=0 cellpadding=5 cellspacing=0>
+	<table border=0 cellpadding=5 cellspacing=0 style="margin-left: 3em;">
 	  <tr><td><a href="http://vogons.zetafleet.com/"><b>VOGONS :: Very Old Games On New Systems</b></a></td></tr>
 	  <tr>
-	    <td><blockquote>
+	    <td style="padding-left: 3em;">
 	    VOGONS is a large forum site, offering supports and tips for people who need help running older
 	    classic games on their modern computers and operating systems. If you want to play a game that
 	    ScummVM doesn't support, this is the place to ask for help!
-	    </blockquote></td>
+	    </td>
 	  </tr>
 
 	  <tr><td><a href="http://www.mixnmojo.com/"><b>MixNMojo :: The Purple LucasArts Fan Site</b></a></td></tr>
 	  <tr>
-	    <td><blockquote>
+	    <td style="padding-left: 3em;">
 	    MixNmojo is one of the longest running, and definitely largest, LucasArts sites out there. If your looking for
 information, hints, news... or even just a place to hang... with hosted and partnered sites catering for everything, the
 International House of Mojo should be your very first stop.
-	    </blockquote></td>
+	    </td>
 	  </tr>
 
 	  <tr><td><a href="http://www.doublefine.com/news.htm"><b>DoubleFine :: Where taking over the word is a news scripts job</b></a></td></tr>
 	  <tr>
-	    <td><blockquote>
+	    <td style="padding-left: 3em;">
             DoubleFine is the company of the lovable Tim Schafer, the genius behind some of the best of the worst gags and games in 
 LucasArts history. Go buy Psychonauts, their new game. Because if you don't, the rats will
 get you and eat your family, computer and house. And poop everywhere.
-	    </blockquote></td>
+	    </td>
 	  </tr>
 	  <tr><td><a href="http://www.revgames.com/"><b>Revolution Software</b></a></td></tr>
 	  <tr>
-	    <td><blockquote>
+	    <td style="padding-left: 3em;">
 	    Revolution Software are the people behind such wonderful titles as Beneath a Steel Sky and
 	    the Broken Sword series of games. Revolution have provided us with the source code to some
 	    of these games so that we can add support for them, and allowed us to release BASS as freeware. Thanks guys!
-	    </blockquote></td>
+	    </td>
 	  </tr>
 	  <tr><td><a href="http://scummlinux.sourceforge.net/"><b>ScummLinux :: a ScummVM LiveCD</b></a></td></tr>
 	    <tr>
-	      <td><blockquote>
+	      <td style="padding-left: 3em;">
 	      ScummLinux allows you to play your favorite ScummVM-supported games anywhere; 
 	      just boot the CD, choose a game, and enjoy.  It supports all the soundcards 
 	      supported by the Linux kernel and TV-Out for some graphics cards, so you can
 	      even play on a television set.
-              </blockquote></td>
+              </td>
 	  </tr>
 
 	</table>
-	</blockquote>
 	
 	<!-- -->
 
 	<h2>Other classic game engine open source projects</h2>
 	<p>The following are links to other classic game engine open source projects similar to ScummVM. 
       Know any other classic game projects that should be linked here?
-      <a href="http://sourceforge.net/sendmessage.php?touser=339357" target="_blank">Let me know!</a>
+      <a href="http://sourceforge.net/sendmessage.php?touser=339357">Let me know!</a>
     </p>
 
-	<blockquote>
-	<table border=0 cellpadding=5 cellspacing=0>
+	<table border=0 cellpadding=5 cellspacing=0 style="margin-left: 3em;">
 	  <tr><td><a href="http://freesci.linuxgames.com/"><b>FreeSCI :: Sierra On-Line SCI Games</b></a></td></tr>
 	  <tr>
-	    <td><blockquote>
+	    <td style="padding-left: 3em;">
 	    The FreeSCI project is an attempt to create a portable interpreter written for Sierra
 	    On-Line's SCI game system. This engine was used in such games as: <i>King's Quest IV,
 	    Leisure Suit Larry III, Space Quest III,</i> and many more.
-	    </blockquote></td>
+	    </td>
 	  </tr>
 
 
 	  <tr><td><a href="http://sarien.sourceforge.net/"><b>Sarien :: Sierra On-Line AGI Games</b></a></td></tr>
 	  <tr>
-	    <td><blockquote>
+	    <td style="padding-left: 3em;">
 	    Sarien is an open source, portable implementation of the Sierra On-Line Adventure Game
 	    Interpreter (AGI). This engine was used in such games as: <i>King's Quest I - III,
 	    Leisure Suit Larry I, Space Quest I - II, Police Quest I,</i> and many more.
-	    </blockquote></td>
+	    </td>
 	  </tr>
 
 	  <tr><td><a href="http://xu4.sourceforge.net/"><b>XU4 :: Ultima IV</b></a></td></tr>
 	  <tr>
-	    <td><blockquote>
+	    <td style="padding-left: 3em;">
 	    XU4 is the recreation of Ultima IV - Quest of the Avatar.
-	    </blockquote></td>
+	    </td>
 	  </tr>
 
 	  <tr><td><a href="http://nuvie.sourceforge.net/"><b>Nuvie :: Ultima VI</b></a></td></tr>
 	  <tr>
-	    <td><blockquote>
+	    <td style="padding-left: 3em;">
 	    Nuvie, pronounced 'New-Vee' is an open sourced game engine for playing Origin's games
 	    Ultima 6, Martian Dreams and Savage Empire on modern operating systems.
-	    </blockquote></td>
+	    </td>
 	  </tr>
 
 	  <tr><td><a href="http://exult.sourceforge.net/"><b>Exult :: Ultima VII</b></a></td></tr>
 	  <tr>
-	    <td><blockquote>
+	    <td style="padding-left: 3em;">
 	    Exult is the recreation of the Ultima VII engine used in <i>The Black Gate</i>
 	    and <i>The Serpent Isle</i>.
-	    </blockquote></td>
+	    </td>
 	  </tr>
 
 	  <tr><td><a href="http://sourceforge.net/projects/yarn/"><b>Yarn :: Dreamer's Guild SAGA Games</b></a></td></tr>
 	  <tr>
-	    <td><blockquote>
+	    <td style="padding-left: 3em;">
 	    Yarn is an open source replacement for the SAGA adventure-game engine used for some of Dreamer's Guild's games including <i>Inherit The Earth</i> and <i>I Have No Mouth And I Must Scream</i>.
-	    </blockquote></td>
+	    </td>
 	  </tr>
 	</table>
-	</blockquote>
     
 	<!-- -->
 
@@ -258,34 +254,32 @@ get you and eat your family, computer and house. And poop everywhere.
 	be quite difficult at times. Luckily there are some sites that provide you
 	with a bunch of information about SCUMM; here are some of them.
 	</p>
-	<blockquote>
-	<table border=0 cellpadding=5 cellspacing=0>
+	<table border=0 cellpadding=5 cellspacing=0 style="margin-left: 3em;">
 
 	  <tr><td><a href="http://www.cowlark.com/scumm/"><b>The inCompleat SCUMM Reference Guide</b></a></td></tr>
 	  <tr>
-	    <td><blockquote>
+	    <td style="padding-left: 3em;">
 	    Information on the file format used by SCUMM five and six.
-	    </blockquote></td>
+	    </td>
 	  </tr>
     
 	  <tr><td><a href="http://scummrev.mixnmojo.com/"><b>SCUMM Revisited</b></a></td></tr>
 	  <tr>
-	    <td><blockquote>
+	    <td style="padding-left: 3em;">
 	    Windows viewer for SCUMM data files. The site also has information on the 
 	    file formats used by the various SCUMM versions.
-	    </blockquote></td>
+	    </td>
 	  </tr>
     
 	  <tr><td><a href="http://scumm.mixnmojo.com/"><b>LucasHacks</b></a></td></tr>
 	  <tr>
-	    <td><blockquote>
+	    <td style="padding-left: 3em;">
 	    Contains lots of information about all SCUMM games, links to demos and utilities,
 	    some documents on SCUMM internals and other useful things.
-	    </blockquote></td>
+	    </td>
 	  </tr>
 
 	</table>
-	</blockquote>
 
 	<h2>GUI frontends for ScummVM</h2>
 	<p>
@@ -293,27 +287,16 @@ Although ScummVM now includes a basic graphical user interface for adding and co
 incomplete. For those whom are not comfortable using the command line to access the more advanced options, here are some other
 GUI frontends for ScummVM.
 	</p>
-	<blockquote>
-	<table border=0 cellpadding=5 cellspacing=0>
+	<table border=0 cellpadding=5 cellspacing=0 style="margin-left: 3em;">
 
 	  <tr><td><a href="http://www.d.kth.se/~d00-ogo/ogosoft/scummaqua/"><b>ScummAqua</b></a></td></tr>
 	  <tr>
-	    <td><blockquote>
+	    <td style="padding-left: 3em;">
 	    For Mac OS X.
-	    </blockquote></td>
+	    </td>
 	  </tr>
 
 	</table>
-	</blockquote>
-    
-
-	<div align="center">
-	<table width="300" border="0"><tr><td>
-	  <img src="images/scummvm-link.png" width="88" height="31" alt="ScummVM" align="left">
-	  <b>Link to us:</b><br>
-	  If you want to link your site to us. Please feel free to use this image.
-	</td></tr></table>
-	</div>
 		
 <?
 
