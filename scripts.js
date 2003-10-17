3779cd0884050496e75f728937a6bbd220d605b3
Max Horn <max@quendi.de>
2003-10-17 19:29:56 +0000

    some more tweaking, trying to accomodate various quirky browser (= basically all of them :-)
    svn path=/web/trunk/; revision=1513

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 
 function openWin(fileToOpen,nameOfWindow,width,height) {
-	myWindow = window.open("",nameOfWindow,"menubar=no,scrollbars=no,status=no,innerWidth="+width+",innerHeight="+height);
+	myWindow = window.open("",nameOfWindow,"menubar=no,scrollbars=no,status=no,width="+width+",height="+height);
 	myWindow.document.open();
 	myWindow.document.write('<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN">\n')
 	myWindow.document.write('<html><head><title>ScreenShot Viewer</title><link rel="stylesheet" href="styles.css" type="text/css"></head>')
