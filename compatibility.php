00152966c9b20b99414a4c3a6da3fd6819c89a35
Travis Howell <kirben@optusnet.com.au>
2003-12-17 11:35:47 +0000

    Add bs1
    svn path=/web/trunk/; revision=1545

--- compatibility.php
+++ compatibility.php
@@ -95,6 +95,7 @@ $games = array(
 		'The Dig'                                               => array('7','dig','85'),
 		'Curse of Monkey Island'				=> array('8','comi','80'),
 		'Beneath a Steel Sky'			       		=> array('n/a','sky','90'),
+		'Broken Sword I'			       		=> array('n/a','sword1','10'),
 		'Broken Sword II'			       		=> array('n/a','sword2','70'),
 		'Flight of the Amazon Queen'			       	=> array('n/a','queen','70'),
  		'Simon The Sorcerer 1 Talkie (Win)'			=> array('n/a','simon1win','95'),
@@ -227,6 +228,9 @@ $notes = array(
 		   "<br>&nbsp;&nbsp;&nbsp;&nbsp;This is especcially noticable in the court- and Mrs. Piermont sequence.".
 		   "<br>- The fonts for the LINC terminal are partially incorrect and the text sometimes passes the screen borders".
 		   "<br>- Special characters for french and italian subtitles are incorrect sometimes",
+"sword1"	=> "Game should run up to Police Station point, but not completable".
+		   "<br>- No load/save".
+		   "<br>- No cutscenes",
 "sword2"	=> "Game is completable, with several minor glitches".
 		   "<br>- Does not work on systems that require alignment".
 		   "<br>- No video for cutscenes",
