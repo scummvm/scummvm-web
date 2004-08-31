ca75e2737cc6c9e4deda908f8497e2a3a82fcac6
James Brown <ender@users.sourceforge.net>
2004-08-31 14:13:19 +0000

    Add new article
    svn path=/web/trunk/; revision=1822

--- press.php
+++ press.php
@@ -22,12 +22,15 @@ echo html_round_frame_start("Press Coverage","");
 <h1>ScummVM press coverage</h1>
 <small>(If you wish to contact us in regards to media articles, please e-mail press 
 (@) scummvm.org)</small>
-	<p><a href="http://www.pcplus.co.uk/tutorials/default.asp?pagetypeid=2&articleid=29821&subsectionid=376&subsubsectionid=749"
-	  ><b>Back to the Source</b></a>, PC Plus issue 215, May 2004
+	<p><a href="http://www.micromart.co.uk/content/features/default.asp?Category=Article&Type=&ID=1746">
+	  <b>Retro-Mart</b></a>, www.micromart.co.uk
+	</p>
+	<p><a href="http://www.pcplus.co.uk/tutorials/default.asp?pagetypeid=2&articleid=29821&subsectionid=376&subsubsectionid=749">
+	  <b>Back to the Source</b></a>, PC Plus issue 215, May 2004
 	</p>
 
-	<p><a href="http://www.mac-emu.net/article.php3?id_article=780"
-	   ><b>Dossier ScummVM</b></a> (French), mac-emu, April 2004
+	<p><a href="http://www.mac-emu.net/article.php3?id_article=780"><b>Dossier ScummVM</b></a>
+	   (French), mac-emu, April 2004
 	</p>
 
 	<p><a href="http://www.techtv.com/callforhelp/freefile/story/0,24330,3640615,00.html">
