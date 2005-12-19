45d691b2c636f031e6d6379ce88b7fea30256889
Max Horn <max@quendi.de>
2005-12-19 02:23:02 +0000

    New credits format (DocBook conversion not yet done)
    svn path=/web/trunk/; revision=2441

--- credits.php
+++ credits.php
@@ -7,6 +7,7 @@
 
 // set this for position of this file relative
 $file_root = ".";
+  global $file_root;
 
 // load libraries
 require($file_root."/include/"."incl.php");
@@ -16,21 +17,7 @@ html_page_header('ScummVM :: Credits');
 
 html_content_begin('Credits');
 
-?>
-<div class="par-item">
-  <div class="par-head">
-    Credits
-  </div>
-  <div class="par-content">
-
-  <div class="credits-table">
-<?php
-
 include $file_root."/credits.inc";
-
-echo "     </div>\n";
-echo "  </div>\n";
-echo "</div>\n";
 
 html_content_end();
 html_page_footer();
