29a0c7c0eb2d4b4326ee130d722dee4fd9144ac6
Max Horn <max@quendi.de>
2006-08-05 23:08:52 +0000

    Use CSS to fatten the first letter in our menus
    svn path=/web/trunk/; revision=23683

--- include/menu.php
+++ include/menu.php
@@ -3,51 +3,30 @@ class htmlmenu {
 
   function htmlmenu($name, $style, $extra = "") {
     global $file_root;
-    echo '<table class="menu" cellspacing="0">';
 ?>
+  <table class="menu" cellspacing="0">
   <tfoot>
     <tr>
       <td><img src="<?=$file_root?>/images/menu-bottom.gif" alt="" width="145" height="13" /></td>
     </tr>
-
 <?php
      if ($extra) {
-       echo "<tr><td>$extra</td></tr>\n";
+       echo "<tr><td>$extra</td></tr>";
      }
 ?>
   </tfoot>
-
-<?php
-    echo '<tr class="'.$style.'">';
-    echo "  <th>$name</th>";
-?>
-
-  </tr>
-  <tbody>
-    <tr>
-      <td>
-        <ul>
+  <tr class="<?=$style?>"><th><?=$name?></th></tr>
+  <tbody><tr><td><ul>
 <?php
  }
 
  /* add a table row */
- function add($name, $url = null) {
-   if($url) {
-     echo "          <li><a href='$url'><b>{$name[0]}</b>".substr($name, 1)."</a></li>";
-   } else {
-     echo "          <li><b>{$name[0]}</b>".substr($name, 1)."</li>";
-   }
+ function add($name, $url) {
+     echo "<li><a href='$url'>$name</a></li>";
  }
 
  function done() {
-?>
-        </ul>
-      </td>
-    </tr>
-  </tbody>
-</table>
-
-<?php
+    echo "</ul></td></tr></tbody></table>";
  }
 }
 ?>
