9b0ba5dcfe091c825254513eea42759daf7975de
Max Horn <max@quendi.de>
2006-09-20 16:35:23 +0000

    Added DTD URL to doctype; specify the scripting language in a meta tag
    svn path=/web/trunk/; revision=23948

--- include/html.php
+++ include/html.php
@@ -4,10 +4,12 @@ function html_page_header($title, $extra_css = array()) {
   global $file_root;
 
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+        "http://www.w3.org/TR/html4/loose.dtd">
 <html lang="en">
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+		<meta http-equiv="Content-Script-Type" content="text/javascript">
 		<link rel="stylesheet" href="<?=$file_root?>/style.css" type="text/css">
 
 <?php
