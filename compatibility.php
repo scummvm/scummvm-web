824dbce6f3990a2f675463c37208e80cca7bae42
Gregory Montoir <cyx@scummvm.org>
2007-03-02 23:59:44 +0000

    update
    svn path=/web/trunk/; revision=25945

--- compatibility.php
+++ compatibility.php
@@ -307,7 +307,7 @@ $notes = array(
 		   "<br>- PlayStation 1 version isn't supported.",
 "queen"		=> "No known issues, game is completable.".
 		   "<br>- Some versions may require the <a href=\"https://svn.sourceforge.net/svnroot/scummvm/engine-data/trunk/queen.tbl\">queen.tbl</a> resource file to be placed in the game directory. This is not required for the freeware releases.".
-		   "<br>- Amiga versions aren't supported.",
+		   "<br>- Amiga and DOS versions supported by this target.",
 "gob1"		=> "No known issues. Game is completable.".
 		   "<br>- Amiga, Atari, DOS and Macintosh versions are supported by this target".
 		   "<br>- Problem with one music piece in the Macintosh version",
