680435d4488dc79a9677e1cac6bc1f6d28c0cf1b
Max Horn <max@quendi.de>
2007-03-05 11:44:18 +0000

    Added google analytics code
    svn path=/web/trunk/; revision=25992

--- include/html.php
+++ include/html.php
@@ -161,6 +161,12 @@ function html_page_footer() {
 			</table>
 		</td></tr>
 		</table>
+<script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
+</script>
+<script type="text/javascript">
+_uacct = "UA-1455743-1";
+urchinTracker();
+</script>
 	</body>
 </html>
 
