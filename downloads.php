b732cbb163b27fb906425a5e0244b4a5921ce30c
Max Horn <max@quendi.de>
2007-06-21 17:40:00 +0000

    0.10.0 is not in Debian stable
    svn path=/web/trunk/; revision=27591

--- downloads.php
+++ downloads.php
@@ -85,7 +85,7 @@ html_content_begin('Download ScummVM');
 
 	<p>For a list of changes since the previous version, <a href="http://sourceforge.net/project/shownotes.php?release_id=516640">read the release notes</a>.
 	<BR>
-	0.10.0 should be directly apt-get'able from Debian stable (etch) and Debian unstable (sid).
+	0.10.0 should be directly apt-get'able from Debian unstable (sid).
 	</p>
 
 <!-- ' Relax XEmacs syntax highlighting -->
