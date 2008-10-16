b2ce054d78024ab1787c669b7d6894c540c1b44b
Paweł Kołodziejski <aquadran@scummvm.org>
2008-10-16 12:48:31 +0000

    cleanup
    svn path=/web/trunk/; revision=34810

--- subprojects.php
+++ subprojects.php
@@ -65,12 +65,9 @@ as such is progressing slowly.
 <P>
 You can try compiled engine for:
 </P>
-<p>Windows:</p>
-<a href="downloads/residualwin32.exe">Windows Snapshot</a> <small> (build from Subversion trunk, <?php echo intval(filesize("downloads/residualwin32.exe")/1024) ?>K Win32 .exe file, last update: <?php echo date("F j, Y, g:i a",filemtime("downloads/residualwin32.exe")); ?>)</small>
-<p>Mac OS X Leopard version on Intel platform:</p>
-<a href="downloads/Residual-MacOSX-Intel.dmg">Mac OS X Snapshot</a> <small> (build from Subversion trunk, <?php echo intval(filesize("downloads/Residual-MacOSX-Intel.dmg")/1024) ?>K dmg file, last update: <?php echo date("F j, Y, g:i a",filemtime("downloads/Residual-MacOSX-Intel.dmg")); ?>)</small>
-<p>Debian Etch on i386 compatible platform:</p>
-<a href="downloads/residual_etch_i386.deb">Debian Etch i386 Snapshot</a> <small> (build from Subversion trunk, <?php echo intval(filesize("downloads/residual_etch_i386.deb")/1024) ?>K dmg file, last update: <?php echo date("F j, Y, g:i a",filemtime("downloads/residual_etch_i386.deb")); ?>)</small>
+<a href="downloads/residualwin32.exe">Windows</a> <small> (build from Subversion trunk, <?php echo intval(filesize("downloads/residualwin32.exe")/1024) ?>K Win32 .exe file, last update: <?php echo date("F j, Y, g:i a",filemtime("downloads/residualwin32.exe")); ?>)</small>
+<a href="downloads/Residual-MacOSX-Intel.dmg">Mac OS X Intel</a> <small> (build from Subversion trunk, <?php echo intval(filesize("downloads/Residual-MacOSX-Intel.dmg")/1024) ?>K dmg file, last update: <?php echo date("F j, Y, g:i a",filemtime("downloads/Residual-MacOSX-Intel.dmg")); ?>)</small>
+<a href="downloads/residual_etch_i386.deb">Debian Etch i386</a> <small> (build from Subversion trunk, <?php echo intval(filesize("downloads/residual_etch_i386.deb")/1024) ?>K deb file, last update: <?php echo date("F j, Y, g:i a",filemtime("downloads/residual_etch_i386.deb")); ?>)</small>
 <p></p>
     </div>
   </div>
