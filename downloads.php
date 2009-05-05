967c4030b6b61791ba0f9fd4b2411ff7baa14f60
Travis Howell <kirben@optusnet.com.au>
2009-05-05 09:40:06 +0000

    Ubuntu 9.04 (jaunty) builds don't actually exist.
    svn path=/web/trunk/; revision=40314

--- downloads.php
+++ downloads.php
@@ -128,8 +128,10 @@ function addDownload($image, $linkText, $desc, $url) {
 	addDownload("catpl-ubuntu", "Ubuntu 8.10 (intrepid) i386 package", "(3.7M .deb)", "http://prdownloads.sourceforge.net/scummvm/scummvm_$current_release-intrepid.1_i386.deb?download");
 	addDownload("catpl-ubuntu", "Ubuntu 8.10 (intrepid) x86 64bit package", "(4.1M .deb)", "http://prdownloads.sourceforge.net/scummvm/scummvm_$current_release-intrepid.1_amd64.deb?download");
 
+/*
 	addDownload("catpl-ubuntu", "Ubuntu 9.04 (jaunty) i386 package", "(3.7M .deb)", "http://prdownloads.sourceforge.net/scummvm/scummvm_$current_release-0.jaunty.1_i386.deb?download");
 	addDownload("catpl-ubuntu", "Ubuntu 9.04 (jaunty) x86 64bit package", "(4.1M .deb)", "http://prdownloads.sourceforge.net/scummvm/scummvm_$current_release-0.jaunty.1_amd64.deb?download");
+*/
 
 	addDownload("catpl-slackware", "SlackWare package", "(3.6M .tgz)", "http://prdownloads.sourceforge.net/scummvm/scummvm-".$current_release."_slack-i486-1.tgz?download");
 
