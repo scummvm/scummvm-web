0bf14d74bac2c00f4326a35df651084c42081264
Travis Howell <kirben@optusnet.com.au>
2009-05-17 08:03:51 +0000

    Ooops, add version requirement for RTZ demo.
    svn path=/web/trunk/; revision=40637

--- demos.php
+++ demos.php
@@ -437,7 +437,7 @@ $MISC_demos = array(
 		=> array('http://demos.robertmegone.com/parallaction/nippon-amiga-demo-en.zip', 'nippon'),
 	'Operation Stealth (Amiga demo - Non interactive)'
 		=> array('http://demos.robertmegone.com/cine/op-amiga-demo-en.zip', 'os'),
-	'Return to Zork (DOS demo - Non interactive)'
+	'Return to Zork (DOS demo - Non interactive) - Requires ScummVM 0.14.0svn'
 		=> array('http://demos.robertmegone.com/made/rtz-dos-ni-demo-en.zip', 'rtz'),
 	'The Legend of Kyrandia (DOS demo - Non-interactive)'
 		=> array('http://demos.robertmegone.com/kyra/kyra1-dos-ni-demo-en.zip', 'kyra1'),
