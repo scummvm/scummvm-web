31a61a1e1d95db4a3c8a5a7fc96e69ed5c44bbef
Eugene Sandulenko <sev@scummvm.org>
2011-05-31 14:41:21 +0000

    WEB: 1.3.0 is in Debian unstable
    svn path=/web/trunk/; revision=56063

--- include/config.inc.php
+++ include/config.inc.php
@@ -2,7 +2,7 @@
 /* Current version. */
 define('RELEASE', '1.3.0');
 define('RELEASE_TOOLS', '1.3.0');
-define('RELEASE_DEBIAN', '1.2.1');
+define('RELEASE_DEBIAN', '1.3.0');
 
 /* News items on the front page. */
 define('NEWS_ITEMS', 4);
