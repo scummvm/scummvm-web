1b9d45b878d104b52be03ac40962f1260e2f70a2
Filippos Karapetis <bluegr@gmail.com>
2013-11-04 12:11:19 +0200

    WEB: Split game downloads into a separate "Games" page
    This separates binary ScummVM downloads from the freeware games and
    extras downloads

---
+++ include/Pages/GamesPage.php
@@ -0,0 +1,33 @@
+<?php
+require_once('Controller.php');
+require_once('Models/GamesModel.php');
+
+class GamesPage extends Controller {
+	private $_template;
+
+	/* Constructor. */
+	public function __construct() {
+		parent::__construct();
+		$this->_template = 'games.tpl';
+	}
+
+	/* Display the index page. */
+	public function index() {
+		$downloads = GamesModel::getAllDownloads();
+		$sections = GamesModel::getAllSections();
+
+		$this->addCSSFiles('downloads.css');
+		return $this->renderPage(
+			array(
+				'title' => 'Games',
+				'content_title' => 'Download freeware games',
+				'downloads' => $downloads,
+				'sections' => $sections,
+				'release_tools' => RELEASE_TOOLS,
+				'release_debian' => RELEASE_DEBIAN,
+			),
+			$this->_template
+		);
+	}
+}
+?>
