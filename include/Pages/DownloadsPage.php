0de80e5bf37bf3c9dedb40014081336868740337
Thierry Crozat <criezy@scummvm.org>
2016-03-28 22:27:52 +0100

    WEB: Fix text for recommended download on Windows

--- include/Pages/DownloadsPage.php
+++ include/Pages/DownloadsPage.php
@@ -28,7 +28,7 @@ class DownloadsPage extends Controller {
 						if (is_array($data)) {
 							$extra_text = $data['size'] . " ";
 							if ($data['ext'] == '.exe')
-								$extra_text = $extra_path + 'Win32 ';
+								$extra_text = $extra_text . 'Win32 ';
 
 							$extra_text .= $data['ext'] . " " . $data['msg'];
 						} else {
