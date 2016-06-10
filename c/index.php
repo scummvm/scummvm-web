a771098a36510a4b33060beef5773322b41a5ab3
Eugene Sandulenko <sev@scummvm.org>
2016-06-10 14:53:57 +0200

    WEB: Adding Cloud redirects WIP.
    Current key are personal, and work only for Tkachov.
    
    Once we put it into production, the keys will be replaced with ones
    generated by sev.

---
+++ c/index.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html>
+	<head>
+		<title>ScummVM</title>
+		<meta charset="utf-8"/>
+	</head>
+	<body>
+		<?php echo "<h1>".htmlentities($_GET["code"], ENT_QUOTES | ENT_HTML5)."</h1>"; ?>
+	</body>
+</html>
