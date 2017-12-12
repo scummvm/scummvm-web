a42073e4339dc78d2708fafeecf390017a52d5fd
Thierry Crozat <criezy@scummvm.org>
2017-12-12 23:06:01 +0000

    WEB: Update download page for version 2.0.0

--- include/config.inc.php
+++ include/config.inc.php
@@ -1,7 +1,7 @@
 <?php
 /* Current version. */
-define('RELEASE', '1.9.0');
-define('RELEASE_TOOLS', '1.9.0');
+define('RELEASE', '2.0.0');
+define('RELEASE_TOOLS', '2.0.0');
 define('RELEASE_DEBIAN', '1.9.0');
 /* Version when the percentages on the compat page were removed */
 define('COMPAT_LAYOUT_CHANGE', '1.7.0');
