ab929d60a3c82509cda7f3f3df7b9e32e52e1ba8
Matan Bareket <mataniko@gmail.com>
2017-12-31 08:02:17 -0500

    WEB: Replace slimbox with baguetteBox
    Slimbox is unresponsive has a dependancy on jQuery. It was easier to rip it out than to update it

--- javascripts/slimbox/src/autoload.js
+++
@@ -1,6 +0,0 @@
-// AUTOLOAD CODE BLOCK (MAY BE CHANGED OR REMOVED)
-jQuery(function($) {
-	$("a[rel^='lightbox']").slimbox({/* Put custom options here */}, null, function(el) {
-		return (this == el) || ((this.rel.length > 8) && (this.rel == el.rel));
-	});
-});
