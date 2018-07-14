eaf1a4d810c50c78db2bec4d0a2da50b0588cec3
Matan Bareket <mataniko@gmail.com>
2018-07-14 17:50:09 -0400

    WEB: echo composer install in site-update

--- site-update/index.php
+++ site-update/index.php
@@ -2,7 +2,7 @@
 <body>
 <pre>
 <?php
-  echo shell_exec("cd ..;echo '$ git pull --rebase';git pull --rebase;echo '$ git status';git status;composer install");
+  echo shell_exec("cd ..;echo '$ git pull --rebase';git pull --rebase;echo '$ git status';git status;echo '$ composer install';composer install");
 ?>
 </pre>
 </body>
