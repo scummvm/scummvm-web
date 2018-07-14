736b21ae0f4fb8aa73ac9c7324bf4850f0fac0c8
Matan Bareket <mataniko@gmail.com>
2018-07-14 17:50:09 -0400

    WEB: echo composer install in site-update and redirect stderr

--- site-update/index.php
+++ site-update/index.php
@@ -2,7 +2,7 @@
 <body>
 <pre>
 <?php
-  echo shell_exec("cd ..;echo '$ git pull --rebase';git pull --rebase;echo '$ git status';git status;echo '$ composer install';composer install");
+  echo shell_exec("cd ..;echo '$ git pull --rebase';git pull --rebase;echo '$ git status';git status;echo '$ composer install';composer install 2>&1");
 ?>
 </pre>
 </body>
