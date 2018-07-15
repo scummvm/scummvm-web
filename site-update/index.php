034e08d32599da006ca6c6bc18cb1193a28d4e27
Matan Bareket <mataniko@gmail.com>
2018-07-15 09:30:21 -0400

    WEB: Extract site-update to it's own shell script

--- site-update/index.php
+++ site-update/index.php
@@ -2,7 +2,10 @@
 <body>
 <pre>
 <?php
+  // Leaving this in initially in case the new script doesn't run...
   echo shell_exec("cd ..;echo '$ git pull --rebase';git pull --rebase;echo '$ git status';git status;echo '$ composer install';composer install 2>&1");
+
+  echo shell_exec('sh install.sh');
 ?>
 </pre>
 </body>
---
+++ site-update/install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Go to the root folder
+cd ..
+
+# Pull the updated website from github
+echo '$ git pull --rebase'
+git pull --rebase
+echo '$ git status'
+git status
+
+# Run composer install
+echo "$ Composer home:" $COMPOSER_HOME
+export COMPOSER_HOME=/var/www
+echo '$ composer install'
+composer install 2>&1
+ 
