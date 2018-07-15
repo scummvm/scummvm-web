2af7b2a669a1a64258e6f6021547b2b3aa5a359f
Matan Bareket <mataniko@gmail.com>
2018-07-15 09:33:50 -0400

    WEB: Remove unnecessary shell command in site update

--- site-update/index.php
+++ site-update/index.php
@@ -1,10 +1,7 @@
 <html>
 <body>
 <pre>
-<?php
-  // Leaving this in initially in case the new script doesn't run...
-  echo shell_exec("cd ..;echo '$ git pull --rebase';git pull --rebase;echo '$ git status';git status;echo '$ composer install';composer install 2>&1");
-
+<?php  
   echo shell_exec('sh install.sh');
 ?>
 </pre>
