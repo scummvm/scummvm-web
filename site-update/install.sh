377b2abb15b6d5e8700141cfbc9170a704758883
Matan Bareket <mataniko@gmail.com>
2018-07-21 00:54:21 -0400

    WEB: Generate spritesheet when updating site

--- site-update/install.sh
+++ site-update/install.sh
@@ -9,6 +9,9 @@ git pull --rebase
 echo '$ git status'
 git status
 
+# Generate image sprite
+glue images/icons/games/ --img=images/ --scss=scss/sprites/_games.scss
+
 # Run composer install
 export COMPOSER_HOME=/var/www/composer
 echo "$ Composer home:" $COMPOSER_HOME
