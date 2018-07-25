0bafc1bf318d3e7189a2cffdc3eeca393f93fe2c
Matan Bareket <mataniko@gmail.com>
2018-07-25 12:32:27 -0400

    WEB: Build sprites for 2x dpi

--- site-update/install.sh
+++ site-update/install.sh
@@ -10,8 +10,8 @@ echo '$ git status'
 git status
 
 # Generate image sprite
-glue images/icons/games/ --img=images/ --scss=scss/sprites/
-glue images/icons/platforms/ --img=images/ --scss=scss/sprites/
+glue images/icons/games/ --img=images/ --scss=scss/sprites/ --retina
+glue images/icons/platforms/ --img=images/ --scss=scss/sprites/ --retina
 mv scss/sprites/games.scss scss/sprites/_games.scss
 mv scss/sprites/platforms.scss scss/sprites/_platforms.scss
 
