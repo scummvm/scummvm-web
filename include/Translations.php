4503353d259384c6ee975a3a36f6bba6708f385d
Matan Bareket <mataniko@gmail.com>
2018-11-06 10:28:03 -0500

    I18N: Update translations script to sync news to/from i18n files

--- include/Translations.php
+++ include/Translations.php
@@ -4,6 +4,8 @@ require_once('vendor/ezyang/htmlpurifier/library/HTMLPurifier.auto.php');
 
 set_include_path('include');
 require_once('Objects/News.php');
+require_once('Models/NewsModel.php');
+
 define('DIR_NEWS', 'data/news');
 
 class Translations {
@@ -24,39 +26,55 @@ class Translations {
 
 		file_put_contents('lang/' . $lang . '.ini', $output);
   }
-  
-  public function xml2json($lang) {    
-    $news = $this->getAllNews($lang);
-    $newsJson = new stdClass();
-    foreach ($news as $key => $value) {
-      $objKey = $value->getFilename();           
-      $newsJson->$objKey = array(
-        "name" => $value->getTitle(),
-        "date" => $value->getDate(),
-        "author" => $value->getAuthor(),
-        "content" => preg_replace('/( {2,})/', ' ', preg_replace('/(\\r)|(\\n)|(\\t)/', ' ', trim($value->getContent())))        
-      );      
-    }
 
-    file_put_contents('data/news/news.'  . $lang . '.json', json_encode($newsJson, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES |  JSON_UNESCAPED_UNICODE));
+  public function updateNewsI18n($lang) {
+
+		// For non-english, create/overwrite JSON files from our i18n file
+		if ($lang != 'en') {
+			$i18n = json_decode(file_get_contents(DIR_NEWS . "/i18n/news.{$lang}.json"));
+
+			foreach ($i18n as $key => $value) {
+				$originalJson = json_decode(file_get_contents(DIR_NEWS . "/{$key}"));
+				$value->date = $originalJson->date;
+				$value->author = $originalJson->author;
+
+				file_put_contents(DIR_NEWS . "/{$lang}/{$key}", json_encode($value, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES |  JSON_UNESCAPED_UNICODE));
+			}
+		} else {
+			// Update the base english i18n file
+      $newsJson = new stdClass();
+			$news = $this->getAllNews($lang);
+			foreach ($news as $key => $value) {
+				$newsJson->$key = array(
+					"title" => $value->title,
+					"content" => $value->content
+				);
+			}
+
+			file_put_contents(DIR_NEWS . "/i18n/news.{$lang}.json", json_encode($newsJson, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES |  JSON_UNESCAPED_UNICODE));
+		}
   }
 
   private function getAllNews($lang) {
-    $dir = DIR_NEWS . "/{$lang}";
+		if ($lang != 'en')
+			$dir = DIR_NEWS . "/{$lang}";
+		else
+			$dir = DIR_NEWS;
+
 		if (!($files = scandir($dir))) {
 			throw new ErrorException(self::NO_FILES);
-		}		
-		$news = array();
+		}
+		$news = new stdClass();
 		foreach ($files as $filename) {
-			if (substr($filename, -4) != '.xml') {
+			if (substr($filename, -5) != '.json') {
 				continue;
-			}		
+			}
       if (!($data = @file_get_contents($dir . "/{$filename}"))) {
         continue;
-      }			
-			$news[] = new News($data, $filename, false);
+      }
+			$news->$filename = json_decode($data);
 		}
-		return array_reverse($news);
+		return $news;
 	}
 }
 
@@ -65,6 +83,6 @@ $jsonDir = 'lang/json/';
 $langs = ['en', 'it', 'fr', 'ru', 'de'];
 foreach ($langs as $key => $value) {
   $translations->json2ini('lang.' . $value);
-  //$translations->xml2json($value);
-}  
+  $translations->updateNewsI18n($value);
+}
 ?>
