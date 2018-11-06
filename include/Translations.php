7e38620d7abdddcf4290152a15465d9ea4c52f2c
Matan Bareket <mataniko@gmail.com>
2018-11-05 23:42:39 -0500

    I18N: Switch News to JSON for I18N

--- include/Translations.php
+++ include/Translations.php
@@ -2,6 +2,10 @@
 
 require_once('vendor/ezyang/htmlpurifier/library/HTMLPurifier.auto.php');
 
+set_include_path('include');
+require_once('Objects/News.php');
+define('DIR_NEWS', 'data/news');
+
 class Translations {
 
 	public function __construct() {
@@ -19,6 +23,48 @@ class Translations {
 		}
 
 		file_put_contents('lang/' . $lang . '.ini', $output);
+  }
+  
+  public function xml2json($lang) {    
+    $news = $this->getAllNews($lang);
+    $newsJson = new stdClass();
+    foreach ($news as $key => $value) {
+      $objKey = $value->getFilename();
+      
+      if ($lang == 'en') {
+        $newsJson->$objKey = array(
+          "name" => $value->getTitle(),
+          "date" => $value->getDate(),
+          "author" => $value->getAuthor(),
+          "content" => preg_replace('/( {2,})/', ' ', preg_replace('/(\\r)|(\\n)|(\\t)/', ' ', trim($value->getContent())))        
+        );
+      } else {
+        $newsJson->$objKey = array(
+          "name" => $value->getTitle(),          
+          "content" => preg_replace('/( {2,})/', ' ', preg_replace('/(\\r)|(\\n)|(\\t)/', ' ', trim($value->getContent())))        
+        );
+      }                  
+    }
+
+    file_put_contents('data/news/json/news.'  . $lang . '.json', json_encode($newsJson, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES |  JSON_UNESCAPED_UNICODE));
+  }
+
+  private function getAllNews($lang) {
+    $dir = DIR_NEWS . "/{$lang}";
+		if (!($files = scandir($dir))) {
+			throw new ErrorException(self::NO_FILES);
+		}		
+		$news = array();
+		foreach ($files as $filename) {
+			if (substr($filename, -4) != '.xml') {
+				continue;
+			}		
+      if (!($data = @file_get_contents($dir . "/{$filename}"))) {
+        continue;
+      }			
+			$news[] = new News($data, $filename, false);
+		}
+		return array_reverse($news);
 	}
 }
 
@@ -26,7 +72,7 @@ $translations = new Translations();
 $jsonDir = 'lang/json/';
 $langs = ['en', 'it', 'fr', 'ru', 'de'];
 foreach ($langs as $key => $value) {
-	$translations->json2ini('lang.' . $value);
-}
-
+  $translations->json2ini('lang.' . $value);
+  //$translations->xml2json($value);
+}  
 ?>
