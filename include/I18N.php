cbe2429431cb6662bfc3672254bec90ee28509c7
Matan Bareket <mataniko@gmail.com>
2018-11-06 16:04:23 -0500

    I18N: Clean up and rename I18N files

--- include/I18N.php
+++ include/I18N.php
@@ -1,22 +1,28 @@
 <?php
 
 require_once('vendor/ezyang/htmlpurifier/library/HTMLPurifier.auto.php');
-
 set_include_path('include');
 require_once('Objects/News.php');
 require_once('Models/NewsModel.php');
-
 define('DIR_NEWS', 'data/news');
 
-class Translations {
+class I18N {
 
 	public function __construct() {
-		$this->_purifier = new HTMLPurifier();
+    $config = HTMLPurifier_Config::createDefault();
+    $this->_purifier = new HTMLPurifier($config);
+
+    $langs = ['en', 'it', 'fr', 'ru', 'de'];
+    foreach ($langs as $key => $value) {
+      $this->convertLanguageJsonToSmartyIni('lang.' . $value);
+      $this->updateNewsI18n($value);
+    }
 	}
 
-	public function json2ini($lang) {
-
-		$jsonString = file_get_contents('lang/json/' . $lang . '.json');
+	private function convertLanguageJsonToSmartyIni($lang) {
+    $filename = "lang/i18n/{$lang}.json";
+    echo("Converting {$filename} from JSON to INI\n");
+		$jsonString = file_get_contents($filename);
 		$json = json_decode($jsonString);
 
 		$output = "";
@@ -24,35 +30,42 @@ class Translations {
 			$output .= $key . " = " . $this->_purifier->purify( $value ). "\n";
 		}
 
-		file_put_contents('lang/' . $lang . '.ini', $output);
+    file_put_contents("lang/{$lang}.ini", $output);
+
   }
 
-  public function updateNewsI18n($lang) {
+  private function updateNewsI18n($lang) {
 
 		// For non-english, create/overwrite JSON files from our i18n file
 		if ($lang != 'en') {
+      echo("Converting " . DIR_NEWS . "/i18n/news.{$lang}.json to individual JSON files\n");
 			$i18n = json_decode(file_get_contents(DIR_NEWS . "/i18n/news.{$lang}.json"));
 
 			foreach ($i18n as $key => $value) {
 				$originalJson = json_decode(file_get_contents(DIR_NEWS . "/{$key}"));
-				$value->date = $originalJson->date;
-				$value->author = $originalJson->author;
+				$value->date = $this->_purifier->purify( $originalJson->date );
+        $value->author = $this->_purifier->purify( $originalJson->author );
+        $value->title = $this->_purifier->purify( $value->title );
+        $value->content = $this->_purifier->purify( $value->content );
 
-				file_put_contents(DIR_NEWS . "/{$lang}/{$key}", json_encode($value, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES |  JSON_UNESCAPED_UNICODE));
+        file_put_contents(DIR_NEWS . "/{$lang}/{$key}", json_encode($value, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES |  JSON_UNESCAPED_UNICODE));
 			}
-		} else {
-			// Update the base english i18n file
+    }
+    else {
+      // Update the base english i18n file
+      echo("Converting individual JSON files to I18N base file\n");
       $newsJson = new stdClass();
 			$news = $this->getAllNews($lang);
 			foreach ($news as $key => $value) {
 				$newsJson->$key = array(
-					"title" => $value->title,
-					"content" => $value->content
+					"title" => $this->_purifier->purify( $value->title ),
+					"content" => $this->_purifier->purify( $value->content )
 				);
 			}
 
 			file_put_contents(DIR_NEWS . "/i18n/news.{$lang}.json", json_encode($newsJson, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES |  JSON_UNESCAPED_UNICODE));
-		}
+    }
+
   }
 
   private function getAllNews($lang) {
@@ -78,11 +91,5 @@ class Translations {
 	}
 }
 
-$translations = new Translations();
-$jsonDir = 'lang/json/';
-$langs = ['en', 'it', 'fr', 'ru', 'de'];
-foreach ($langs as $key => $value) {
-  $translations->json2ini('lang.' . $value);
-  $translations->updateNewsI18n($value);
-}
+new I18N();
 ?>
