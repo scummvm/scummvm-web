de96e75852fb2ffa20e2a726b56ced4ee8713e03
Matan Bareket <mataniko@gmail.com>
2019-01-22 19:55:17 -0500

    I18N: Fix I18N generation

--- include/I18N.php
+++ include/I18N.php
@@ -6,7 +6,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 use ScummVM\Objects\News;
 use ScummVM\Models\NewsModel;
 
-// define('DIR_NEWS', 'data/news');
+define('DIR_NEWS', 'data/news');
 
 class I18N
 {
