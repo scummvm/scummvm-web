2f2a432581a8cc75c13fb3060b2e49549a6b6903
Thunderforge <wjherrmann@gmail.com>
2020-03-26 18:05:32 -0500

    DATA: Converting Game Demos page to YAML
    Since it appears that #110 will require compatibility data to be changed from XML to YAML, I wanted to practice and change a simpler page from XML to YAML. If merged, I'm willing to convert other pages as well (e.g. Links, Press Articles, Screenshots, FAQs) as well

--- include/Models/GameDemosModel.php
+++ include/Models/GameDemosModel.php
@@ -2,7 +2,6 @@
 namespace ScummVM\Models;
 
 use ScummVM\Objects\GameDemo;
-use ScummVM\XMLParser;
 
 /**
  * The GameDemosModel class will generate GameDemo objects.
@@ -12,13 +11,13 @@ abstract class GameDemosModel extends BasicModel
     /* Get all the groups and their respective demos. */
     public static function getAllGroupsAndDemos()
     {
-        $fname = DIR_DATA . '/game_demos.xml';
-        $parser = new XMLParser();
-        $a = $parser->parseByFilename($fname);
+        $fname = DIR_DATA . '/game_demos.yaml';
+        $a = \yaml_parse_file($fname);
+        $entries = array();
         $entries = array();
         foreach ($a['game_demos']['group'] as $key => $value) {
             $demos = array();
-            foreach ($value['demos']['demo'] as $data) {
+            foreach ($value['demos'] as $data) {
                 $demos[] = new GameDemo($data);
             }
             $entries[] = array(
