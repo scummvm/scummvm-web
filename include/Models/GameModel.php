30e9c423d34e93f80dcc57c912c049c7585365ff
Mataniko <mataniko@gmail.com>
2020-09-21 18:20:52 -0400

    WEB: Add Series to game objects

--- include/Models/GameModel.php
+++ include/Models/GameModel.php
@@ -4,6 +4,7 @@ namespace ScummVM\Models;
 use ScummVM\Objects\Game;
 use ScummVM\Models\CompaniesModel;
 use ScummVM\Models\EnginesModel;
+use ScummVM\Models\SeriesModel;
 
 /**
  * The GamesModel is used to cross reference Games across the website
@@ -15,11 +16,12 @@ abstract class GameModel extends BasicModel
     {
         $companies = CompaniesModel::getAllCompanies();
         $engines = EnginesModel::getAllEngines();
+        $series = SeriesModel::getAllSeries();
         $fname = DIR_DATA . '/games.yaml';
         $games = \yaml_parse_file($fname);
         $data = [];
         foreach ($games as $game) {
-            $obj = new Game($game, $engines, $companies);
+            $obj = new Game($game, $engines, $companies, $series);
             $data[$obj->getId()] = $obj;
         }
         return $data;
