23dbf430a695b2f4c7f38bc73ce9961c63ece1ae
Mataniko <mataniko@gmail.com>
2020-09-21 18:18:43 -0400

    WEB: Add Series model and object

---
+++ include/Models/SeriesModel.php
@@ -0,0 +1,24 @@
+<?php
+namespace ScummVM\Models;
+
+use ScummVM\Objects\Series;
+
+/**
+ * The SeriesModel is used to group games into series
+ * mainly for the screenshots page
+ */
+abstract class SeriesModel extends BasicModel
+{
+    /* Get all Platforms from YAML */
+    public static function getAllSeries()
+    {
+        $fname = DIR_DATA . '/series.yaml';
+        $series = \yaml_parse_file($fname);
+        $data = [];
+        foreach ($series as $seriesItem) {
+            $obj = new Series($seriesItem);
+            $data[$obj->getId()] = $obj;
+        }
+        return $data;
+    }
+}
