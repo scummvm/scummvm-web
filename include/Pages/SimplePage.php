e12a47d710a7526b14f417e3ddcb57dee5fc0bed
Mataniko <mataniko@gmail.com>
2020-10-08 00:09:07 -0400

    WEB: Create SimplePage class for pages that don't require customization
    Some of our pages are basic enough that they can just be generated automatically.
    
    This commit extracts 5 such pages to reduce code and complexity

---
+++ include/Pages/SimplePage.php
@@ -0,0 +1,49 @@
+<?php
+namespace ScummVM\Pages;
+
+use ScummVM\Controller;
+use ScummVM\Models\SimpleModel;
+
+class SimplePage extends Controller
+{
+    const FILE_NOT_FOUND = 'The filename %s could not be found';
+    const PAGE_MODELS = [
+        'sponsors' => ['Sponsor', 'sponsors.yaml'],
+        'press' => ['Article', 'press_articles.yaml'],
+        'credits' => ['CreditsSection', 'credits.yaml'],
+        'documentation' => ['Document', 'documentation.yaml'],
+    ];
+
+    private $model;
+    private $key;
+
+    /* Constructor. */
+    public function __construct($key)
+    {
+        parent::__construct();
+        $this->template = "pages/$key.tpl";
+        $this->key = $key;
+        $templateFile = SMARTY_DIR_TEMPLATE . "/$this->template";
+        if (!is_file($templateFile) || !is_readable($templateFile)) {
+            throw new \ErrorException(\sprintf(self::FILE_NOT_FOUND, $templateFile));
+        }
+        if (array_key_exists($key, self::PAGE_MODELS)) {
+            [$model, $data] = self::PAGE_MODELS[$key];
+            $this->model = new SimpleModel($model, $data);;
+        }
+    }
+
+    /* Display the index page. */
+    public function index()
+    {
+        if ($this->model) {
+            $data = $this->model->getAllData();
+        }
+
+        return $this->renderPage([
+            'title' => $this->getConfigVars("{$this->key}Title"),
+            'content_title' => $this->getConfigVars("{$this->key}ContentTitle"),
+            'data' => $data
+        ]);
+    }
+}
