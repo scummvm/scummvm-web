01ae52820ae35ab68edc319315b3008d8c3d1480
Mataniko <mataniko@gmail.com>
2020-12-02 21:49:07 -0500

    BUILD: Move from scssphp to sass

---
+++ build.js
@@ -0,0 +1,24 @@
+const sass = require('sass');
+const fs = require('fs');
+const autoprefixer = require('autoprefixer');
+
+async function renderScss(filename) {
+    const inFile = `./scss/${filename}.scss`;
+    const outFile = `./public_html/css/${filename}.css`;
+
+    console.log(`Converting ${inFile} to css`);
+    const output = sass.renderSync({
+        outputStyle: "compressed",
+        file: inFile,
+
+    });
+
+    console.log(`Applying autoprefixer`);
+    const result = await autoprefixer.process(output.css, { from: undefined });
+
+    console.log(`Writing to ${outFile}`);
+    fs.writeFileSync(outFile, result.css);
+}
+
+renderScss('main_rtl');
+renderScss('main_ltr');
