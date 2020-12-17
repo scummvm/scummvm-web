01c4c9e50cf6837c21238b67c4e7665c9f4adc7b
Mataniko <mataniko@gmail.com>
2020-12-16 21:03:07 -0500

    WEB: Move Game Demos to ORM

---
+++ generated-classes/ScummVM/OrmObjects/Demo.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace ScummVM\OrmObjects;
+
+use ScummVM\OrmObjects\Base\Demo as BaseDemo;
+
+/**
+ * Skeleton subclass for representing a row from the 'demo' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ */
+class Demo extends BaseDemo
+{
+    public function getName()
+    {
+        $gameName = $this->getGame()->getName();
+        $platformName = $this->getPlatform()->getName();
+        $category = $this->getCategory();
+        return "$gameName ($platformName $category Demo)";
+    }
+
+    public function __toString()
+    {
+        return $this->getName();
+    }
+}
