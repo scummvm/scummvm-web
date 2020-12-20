99c2a7bbd3c0f5127cc49c384dfd1801798a6620
Mataniko <mataniko@gmail.com>
2020-12-20 11:11:26 -0500

    WEB: Add all ORM Objects to ScummVM package

---
+++ include/OrmObjects/Downloads.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace ScummVM\OrmObjects;
+
+use ScummVM\OrmObjects\Base\Downloads as BaseDownloads;
+
+/**
+ * Skeleton subclass for representing a row from the 'scummvm_downloads' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ */
+class Downloads extends BaseDownloads
+{
+
+}
---
+++ include/OrmObjects/GameQuery.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace ScummVM\OrmObjects;
+
+use ScummVM\OrmObjects\Base\GameQuery as BaseGameQuery;
+
+/**
+ * Skeleton subclass for performing query and update operations on the 'game' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ */
+class GameQuery extends BaseGameQuery
+{
+
+}
