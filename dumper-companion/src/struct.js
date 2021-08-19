95b8771b7e5722a824bcde49968482e15438677a
djsrv <dservilla@gmail.com>
2021-08-19 10:39:50 -0400

    DUMPER: Fix struct.js

--- dumper-companion/src/struct.js
+++ dumper-companion/src/struct.js
@@ -1,8 +1,36 @@
+/**
+ * Copyright (c) 2016 Aksel Jensen (TheRealAksel at github)
+ * Copyright (c) 2021 ScummVM Team
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/**
+ * This file is adapted from
+ * https://github.com/lyngklip/structjs/blob/master/struct.mjs
+ */
+
+
 /*eslint-env es6*/
-const rechk = /^([<>])?(([1-9]\d*)?([xcbB?hHiIfdsp]))*$/
-const refmt = /([1-9]\d*)?([xcbB?hHiIfdsp])/g
-const str = (v,o,c) => String.fromCharCode(
-    ...new Uint8Array(v.buffer, v.byteOffset + o, c))
+const rechk = /^([<>])?(([1-9]\d*)?([xcbB?hHiIlLfdsp]))*$/
+const refmt = /([1-9]\d*)?([xcbB?hHiIlLfdsp])/g
+const str = (v,o,c) => new Uint8Array(v.buffer, v.byteOffset + o, c)
 const rts = (v,o,c,s) => new Uint8Array(v.buffer, v.byteOffset + o, c)
     .set(s.split('').map(str => str.charCodeAt(0)))
 const pst = (v,o,c) => str(v, o + 1, Math.min(v.getUint8(o), c - 1))
@@ -17,6 +45,8 @@ const lut = le => ({
     H: c=>[c,2,o=>({u:v=>v.getUint16( o,le), p:(v,H)=>v.setUint16( o,H,le)})],
     i: c=>[c,4,o=>({u:v=>v.getInt32(  o,le), p:(v,i)=>v.setInt32(  o,i,le)})],
     I: c=>[c,4,o=>({u:v=>v.getUint32( o,le), p:(v,I)=>v.setUint32( o,I,le)})],
+    l: c=>[c,4,o=>({u:v=>v.getInt32(  o,le), p:(v,i)=>v.setInt32(  o,i,le)})],
+    L: c=>[c,4,o=>({u:v=>v.getUint32( o,le), p:(v,I)=>v.setUint32( o,I,le)})],
     f: c=>[c,4,o=>({u:v=>v.getFloat32(o,le), p:(v,f)=>v.setFloat32(o,f,le)})],
     d: c=>[c,8,o=>({u:v=>v.getFloat64(o,le), p:(v,d)=>v.setFloat64(o,d,le)})],
     s: c=>[1,c,o=>({u:v=>str(v,o,c), p:(v,s)=>rts(v,o,c,s.slice(0,c    ) )})],
@@ -31,9 +61,9 @@ export default function struct(format) {
     while ((m = refmt.exec(format))) { ((r, s, f) => {
         for (let i = 0; i < r; ++i, size += s) { if (f) {fns.push(f(size))} }
     })(...lu(...m.slice(1)))}
-    const unpack_from = (arrb, offs) => {
-        if (arrb.byteLength < (offs|0) + size) { throw errbuf }
-        let v = new DataView(arrb, offs|0)
+    const unpack_from = (arr, offs) => {
+        if (arr.byteLength < (offs|0) + size) { throw errbuf }
+        let v = new DataView(arr.buffer, arr.byteOffset + (offs|0))
         return fns.map(f => f.u(v))
     }
     const pack_into = (arrb, offs, ...values) => {
@@ -48,7 +78,7 @@ export default function struct(format) {
         pack_into(b, 0, ...values)
         return b
     }
-    const unpack = arrb => unpack_from(arrb, 0)
+    const unpack = arr => unpack_from(arr, 0)
     function* iter_unpack(arrb) { 
         for (let offs = 0; offs + size <= arrb.byteLength; offs += size) {
             yield unpack_from(arrb, offs);
