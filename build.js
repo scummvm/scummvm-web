c17269b2c6182858bceb939b51d3225e6f049762
mataniko <mataniko@gmail.com>
2021-08-25 21:24:10 -0400

    WEB: Update dependencies and required build steps

--- build.js
+++ build.js
@@ -1,6 +1,7 @@
 const sass = require('sass');
 const fs = require('fs');
 const autoprefixer = require('autoprefixer');
+const postcss = require('postcss')
 
 async function renderScss(filename) {
     const inFile = `./scss/${filename}.scss`;
@@ -14,7 +15,7 @@ async function renderScss(filename) {
     });
 
     console.log(`Applying autoprefixer`);
-    const result = await autoprefixer.process(output.css, { from: undefined });
+    const result = await postcss([autoprefixer]).process(output.css, { from: undefined });
 
     console.log(`Writing to ${outFile}`);
     fs.writeFileSync(outFile, result.css);
