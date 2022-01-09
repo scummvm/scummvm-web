bea0da3bc4321489844cdc03982bf2fccc57e8e3
Thunderforge <wjherrmann@gmail.com>
2022-01-08 18:36:01 -0600

    WEB: Change how file extra info is generated

--- include/Objects/File.php
+++ include/Objects/File.php
@@ -15,7 +15,6 @@ class File extends BasicObject
     {
         parent::__construct($data);
         $this->category_icon = $data['category_icon'];
-        $this->extra_info = $data['extra_info'] ?? null;
         $this->subcategory = $data['subcategory'] ?? null;
         $this->user_agent = isset($data["user_agent"]) ? $data["user_agent"] : "";
         $this->version = strtolower($data['version'] ?? null);
@@ -80,22 +79,10 @@ class File extends BasicObject
                 }
 
                 $this->extra_info['ext'] = $ext;
-                $this->extra_info['msg'] = $data['extra_msg'];
+                $this->extra_info['date'] = date('F j, Y, g:i a', @filemtime($fname));
+                $this->extra_info['msg'] = isset($data['notes']) ? $data['notes'] : '';
             }
             $this->url = $fname;
-        }
-        /**
-         * Get the filesize/last modified information and put it in
-         * $this->extra_info.
-         */
-        if (isset($attributes['extra_info'])
-            && $attributes['extra_info'] == 'true') {
-            if (is_file($fname) && is_readable($fname)) {
-                $this->extra_info['date'] = date('F j, Y, g:i a', @filemtime($fname));
-                if (!is_null($data['extra_info'])) {
-                    $this->extra_info['info'] = $data['extra_info'];
-                }
-            }
         }
     }
 
