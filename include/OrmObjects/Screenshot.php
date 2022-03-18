ed7290f6f0788236dcc31dc3776eae333a0ba4b6
Thunderforge <wjherrmann@gmail.com>
2022-03-18 17:07:24 -0500

    WEB: Adding a caption to random screenshots
    The caption is a hyperlink that goes to the corresponding game page on https://www.scummvm.org/compatibility/ . This allows users to quickly look up compatibility info as well as see which platforms are supported.

--- include/OrmObjects/Screenshot.php
+++ include/OrmObjects/Screenshot.php
@@ -28,6 +28,7 @@ class Screenshot extends BaseScreenshot
                 $this->files[] = [
                     'filename' => $name,
                     'caption' => $this->getCaption(),
+                    'url' => "compatibility/DEV/" . $this->getGame()->getId()
                 ];
             }
         }
