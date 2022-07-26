91874dc27ce37a0657c57a35155545b37a9e4cb2
Misty De Meo <mistydemeo@gmail.com>
2022-07-25 19:41:02 -0700

    Dumper companion: more Mac extensions

--- dumper-companion/src/Dumper.tsx
+++ dumper-companion/src/Dumper.tsx
@@ -42,7 +42,7 @@ export default class Dumper extends Component<Props, State> {
 
                 <div>
                     <p>Select the game's disk image:</p>
-                    <input disabled={this.state.busy} class="file" type="file" accept=".dsk,.image,.img,.iso,.toast" onInput={this.handleImage.bind(this)}/>
+                    <input disabled={this.state.busy} class="file" type="file" accept=".dsk,.image,.img,.iso,.toast,.cdr,.diskcopy,.dcpy" onInput={this.handleImage.bind(this)}/>
                 </div>
 
                 <div>
