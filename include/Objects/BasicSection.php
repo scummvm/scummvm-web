51ce727edd063019e7d277a74a22139e7ea5d944
Le Philousophe <lephilousophe@users.noreply.github.com>
2025-08-11 18:06:15 +0200

    WEB: Simplify subsections construction
    This will ease typing

--- include/Objects/BasicSection.php
+++ include/Objects/BasicSection.php
@@ -4,25 +4,25 @@ namespace ScummVM\Objects;
 /**
  * The BasicSection class is inherited by all other Sections and houses all common
  * functions.
+ *
+ * @phpstan-consistent-constructor
  */
 abstract class BasicSection extends BasicObject
 {
     protected $title;
     protected $anchor;
     protected $subsections;
-    private $className;
 
     public function __construct($data)
     {
         parent::__construct($data);
         $this->title = $data['title'];
         $this->anchor = $data['anchor'];
-        $this->className = static::class;
         $this->subsections = array();
         if (isset($data['subsection'])) {
             parent::toArray($data['subsection']);
             foreach ($data['subsection'] as $value) {
-                $this->subsections[] = new $this->className($value);
+                $this->subsections[] = new static($value);
             }
         }
     }
